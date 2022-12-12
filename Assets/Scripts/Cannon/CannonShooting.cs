e51163bdfdbd117c09523ac2aae2e3be38b08c92
Jack <jacopo.derosa00@gmail.com>
2022-12-12 07:48:26 +0100

    Started shooting

--- Assets/Scripts/Cannon/CannonShooting.cs
+++ Assets/Scripts/Cannon/CannonShooting.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class CannonShooting : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private CannonBall _cannonBallPrefab;
+    [SerializeField] private Vector3 _maxShootingForce;
+    [SerializeField] private Vector3 _minShootingForce;
+
+
+    private void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space)) StartCharging();
+        if (Input.GetKeyUp(KeyCode.Space)) StopCharging();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void StartCharging()
     {
-        
+
+    }
+
+    private void StopCharging()
+    {
+
     }
 }
